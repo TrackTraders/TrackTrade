a5b18ec24eb6783e84a60b569a9b03100145703c
xrawvelocity <vmfs99@gmail.com>
2021-11-22 15:44:04 -0500

    console.log on login and is logged in

--- TrackTradeBackEnd/routes/auth.js
+++ TrackTradeBackEnd/routes/auth.js
@@ -3,18 +3,18 @@ const router = require("express").Router();
 const passport = require("../config/passport");
 
 router.get("/is-logged-in", (req, res, next) => {
-  console.log("-=-=--=-=-=-=-=-");
-  res.json(req.user);
+    console.log(req, res);
+    res.json(req.user);
 });
 
 router.post("/log-in", passport.authenticate("local"), (req, res, next) => {
-  console.log("-==-=-=-=-=-=-=-");
-  res.status(200).json(req.user);
+    console.log(req, res);
+    res.status(200).json(req.user);
 });
 
 router.get("/log-out", (req, res, next) => {
-  req.logout();
-  res.status(200).json({ msg: "Logged out!" });
+    req.logout();
+    res.status(200).json({ msg: "Logged out!" });
 });
 
 module.exports = router;
